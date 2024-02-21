0fcfec937732f3c93ebc87668f6078d40a9b9d35
Dilshan K B <127595699+Binura22@users.noreply.github.com>
2024-02-21 19:43:03 +0530

    Create PC1.java

---
+++ Y2S2/DSA/DSA lab2/Ex3/PC1.java
@@ -0,0 +1,46 @@
+package Ex3;
+
+public class PC1 {
+	private int queueArr[];
+	private int maxSize;
+	private int rear;
+	private int front;
+	private int noItems;
+	
+	PC1(int s){
+		maxSize = s;
+		queueArr = new int[maxSize];
+		rear = -1;
+		front = 0;
+		noItems = 0;
+	}
+	
+	public boolean isEmpty() {
+		return(noItems == 0);
+		
+	}
+	public boolean isFull() {
+		return(rear == maxSize - 1);
+	}
+	
+	public void insert(int j) {
+		if(isFull()) {
+			System.out.println("Queue is full !");
+		} else {
+			queueArr[++rear] = j;
+			noItems++;
+		}
+		
+	}
+	public int remove() {
+		if(isEmpty()) {
+			System.out.println("Queue is empty !");
+			return -99;
+		} else {
+			noItems--;
+			return(queueArr[front++]);	
+		}
+		
+	}
+
+}
