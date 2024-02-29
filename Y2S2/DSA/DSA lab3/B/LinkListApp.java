41e4c9da51ac0b6453d43483274d077276d8e3ed
Dilshan K B <127595699+Binura22@users.noreply.github.com>
2024-03-01 00:14:26 +0530

    create LinkList.java & MainApp.java

---
+++ Y2S2/DSA/DSA lab3/B/LinkListApp.java
@@ -0,0 +1,16 @@
+package B;
+
+public class LinkListApp {
+
+	public static void main(String[] args) {
+	
+		LinkList l1 = new LinkList();
+		l1.insertFirst("Nipuna", 53.5);
+		
+		l1.displayList();
+		
+		l1.deleteFirst();
+		
+	}
+
+}
